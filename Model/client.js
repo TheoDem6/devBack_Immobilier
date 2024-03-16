10d11989edc91f8520cd0840ef76543a15a12ae7
TheoDem6 <theo.demany@gmail.com>
2024-03-16 14:58:08 +0100

    Ajout fichiers manquant

---
+++ Model/client.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+//book schema definition
+let ClientSchema = new Schema(
+  {
+    nom: { type: String,required: true },
+    mail: { type: String, unique : true, required: true },
+    mdp: { type: String, required: true},
+  }, 
+  { 
+    versionKey: false
+  }
+);
+
+//Exports the client for use elsewhere.
+module.exports = mongoose.model('client', ClientSchema);
